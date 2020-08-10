400ca8ad426cf73186ad55b274e47e0e82e3f3ca
victorprens <vprens12@gmail.com>
2020-08-10 07:50:06 -0500

    Adds pagination to the table

--- src/components/EmployeeHours.jsx
+++ src/components/EmployeeHours.jsx
@@ -6,6 +6,9 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import TableFooter from "@material-ui/core/TableFooter";
+import TablePagination from "@material-ui/core/TablePagination";
+import TablePaginationActions from "./TablePaginationActions";
 import Paper from "@material-ui/core/Paper";
 import Tooltip from "@material-ui/core/Tooltip";
 import IconButton from "@material-ui/core/IconButton";
@@ -20,12 +23,23 @@ const useStyles = makeStyles({
 export default function EmployeeHours(props) {
   const classes = useStyles();
   const { employees, deleteWT } = props;
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   return (
     <React.Fragment>
       <h1 style={{ marginTop: 40 }}>Working Times</h1>
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
+      <TableContainer style={{ marginBottom: 40 }} component={Paper}>
+        <Table className={classes.table} aria-label="custom pagination table">
           <TableHead>
             <TableRow>
               <TableCell>ID</TableCell>
@@ -40,7 +54,13 @@ export default function EmployeeHours(props) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {employees.map((employee) => (
+            {(rowsPerPage > 0
+              ? employees.slice(
+                  page * rowsPerPage,
+                  page * rowsPerPage + rowsPerPage
+                )
+              : employees
+            ).map((employee) => (
               <TableRow key={employee.id}>
                 <TableCell component="th" scope="row">
                   {employee.employeeID}
@@ -67,6 +87,24 @@ export default function EmployeeHours(props) {
               </TableRow>
             ))}
           </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                colSpan={3}
+                count={employees.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: { "aria-label": "rows per page" },
+                  native: true,
+                }}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
         </Table>
       </TableContainer>
     </React.Fragment>
